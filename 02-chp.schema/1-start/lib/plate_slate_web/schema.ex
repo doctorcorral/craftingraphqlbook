bb8357f7ef569a713ea34179ea24f8bfe92de293
Ricardo C <ricardo@suggestic.com>
2018-04-25 20:12:25 -0500

    codebase

--- 02-chp.schema/1-start/lib/plate_slate_web/schema.ex
+++ 02-chp.schema/1-start/lib/plate_slate_web/schema.ex
@@ -8,12 +8,12 @@
 #---
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
-
+  
   query do
     # <<Ignore this for now>>
   end
-
-  object :menu_item do
+  
+ object :menu_item do
     # <<We'll add fields soon>>
   end
 end
