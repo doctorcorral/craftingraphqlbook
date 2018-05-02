4ff17c45ad4977ca94b02e8ebd54c3a71ceff5ec
Ricardo C <ricardo@suggestic.com>
2018-05-01 19:21:11 -0500

    add field :menu_items to query

--- 02-chp.schema/1-start/lib/plate_slate_web/schema.ex
+++ 02-chp.schema/1-start/lib/plate_slate_web/schema.ex
@@ -8,12 +8,12 @@
 #---
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
-  
+
   query do
-    # <<Ignore this for now>>
+    field :menu_items, list_of(:menu_item)
   end
   
- object :menu_item do
+  object :menu_item do
     field :id, :id
     field :name, :string
     field :description, :string
