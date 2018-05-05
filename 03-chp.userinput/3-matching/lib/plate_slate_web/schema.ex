d94db45091b37bb3c960c298d02c31160e80b7c7
Ricardo C <ricardo@suggestic.com>
2018-05-04 20:45:07 -0500

    fix format adding parenthesis

--- 03-chp.userinput/3-matching/lib/plate_slate_web/schema.ex
+++ 03-chp.userinput/3-matching/lib/plate_slate_web/schema.ex
@@ -1,29 +1,27 @@
-#---
+# ---
 # Excerpted from "Craft GraphQL APIs in Elixir with Absinthe",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/wwgraphql for more book information.
-#---
+# ---
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
   alias PlateSlateWeb.Resolvers
 
   query do
-
+    @desc "menu items bien bonitos de nuevo"
     field :menu_items, list_of(:menu_item) do
-      arg :matching, :string
-      resolve &Resolvers.Menu.menu_items/3
+      arg(:matching, :string)
+      resolve(&Resolvers.Menu.menu_items/3)
     end
-
   end
 
   object :menu_item do
-    field :id, :id
-    field :name, :string
-    field :description, :string
+    field(:id, :id)
+    field(:name, :string)
+    field(:description, :string)
   end
-
 end
